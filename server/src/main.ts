e43434722e1a4ffc888c96fd545f4eae28b20b5e
zbay <zlwilliams1@gmail.com>
2019-03-05 22:57:51 -0500

    Moved some header security logic from Node to Nginx. Don't need Helmet library

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,63 +4,28 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import * as helmet from 'helmet';
 import { join, resolve } from 'path';
 import * as rateLimit from 'express-rate-limit';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
   const ENV = AppModule.env;
-  const app = await NestFactory.create(AppModule
-    , { cors: {
-        origin: ENV !== `prod` ? `*` : `${AppModule.host}:${AppModule.port}`,
-        preflightContinue: false
-      }
-    }
-  );
   const hostDomain = `${AppModule.host}:${AppModule.port}`;
+  app.enableCors({
+      origin: ENV === `dev` ? `*` : `${AppModule.host}`,
+      preflightContinue: false
+  });
   const hostname = hostDomain.split(`//`)[1];
 
   const morgan = require(`morgan`);
   app.use(morgan(`combined`));
 
   if (ENV !== `dev`) {
-    app.use(helmet({
-        // the following middlewares are specified because they're not enabled by default
-        contentSecurityPolicy: { // remove or change this if using a CDN
-          directives: {
-            defaultSrc: [`'self'`],
-            childSrc: [`'self'`],
-            connectSrc: [`'self'`],
-            fontSrc: [`'self'`, `https://fonts.googleapis.com`],
-            imgSrc: [`'self'`],
-            scriptSrc: [`'self'`],
-            styleSrc: [`'self'`]
-          }
-        },
-        featurePolicy: {
-          features: {
-            accelerometer: [`'none'`],
-            autoplay: [`'none'`],
-            camera: [`'none`],
-            fullscreen: [`'self'`],
-            geolocation: [`'none'`],
-            gyroscope: [`'none'`],
-            magnetometer: [`'none'`],
-            microphone: [`'self'`],
-            midi: [`'none'`],
-            payment: [`'none`],
-            speaker: [`'self'`],
-            syncXhr: [`'none'`],
-            usb: [`'none`],
-            vibrate: [`'none'`],
-            vr: [`'none'`]
-          }
-        }
-      }));
     app.use(rateLimit({
-      windowMs: 60 * 1000, // > 30 requests per minute is prohibited
+      windowMs: 60 * 1000, // > 30 requests per minute per IP address is prohibited
       max: 30
     }));
   }
