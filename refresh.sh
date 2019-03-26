116b093e0f38c882cd89a82014d5f994b5e7ddb7
zbay <zlwilliams1@gmail.com>
2019-03-26 17:56:28 -0400

    Script for refreshing and restarting the app

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git pull https://github.com/zbay/chartman 
+sudo docker pull zbayindustries/chartman_nest 
+sudo docker pull zbayindustries/chartman_frontend
+echo y | sudo docker container prune
+echo y | sudo docker image prune
+sudo docker-compose restart & disown
